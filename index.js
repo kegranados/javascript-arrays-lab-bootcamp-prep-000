bcc2fa56cf0838e0b3ed65cf5c4548341b870666
kegranados1@gmail.com <kegranados1@gmail.com>
2018-07-22 00:37:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 var kittens = ['Milo', 'Otis', 'Garfield']
 
 function destructivelyAppendKitten (name) {
-  name = 'Ralph'
-  kitten.push(name)
-  return kittens
+  kitten = kitten.push(name)
+  return kitten
 }
