b6f2d610bac42de93d12f0cab1e24217b445e368
kegranados1@gmail.com <kegranados1@gmail.com>
2018-07-22 01:05:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,5 @@ function destructivelyRemoveFirstKitten() {
   return kittens
 }
 function appendKitten(name) {
-  kittens = [...kittens, name]
-  return kittens
+  return kittens = [...kittens, name]
 }
