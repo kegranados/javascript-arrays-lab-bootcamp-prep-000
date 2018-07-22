2c374c70994527b032b46af980cae5c3ff55aa5a
kegranados1@gmail.com <kegranados1@gmail.com>
2018-07-22 00:49:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add your functions and code here
-var kitten = ['Milo', 'Otis', 'Garfield']
-name = 'Ralph'
+var kittens = ['Milo', 'Otis', 'Garfield']
+
 function destructivelyAppendKitten(name) {
-  kitten.push(name)
-  return kitten
+  kittens.push(name)
+  return kittens
 }
